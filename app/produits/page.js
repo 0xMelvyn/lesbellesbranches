bc81d2ce6211da289c99441568daa362370bc062
Melvyn <melvynhoarau@hotmail.fr>
2024-08-17 12:54:49 +0200

    galerie

--- app/produits/page.js
+++ app/produits/page.js
@@ -1,7 +1,45 @@
-export default function Home() {
+import Image from 'next/image';
+import PropTypes from 'prop-types';
+
+// Composant Gallery
+const Gallery = ({ images }) => {
   return (
-    <main>
-        <p>Work in progress</p>
-    </main>
+    <div className="grid grid-cols-2 lg:grid-cols-3 gap-4 py-4 p-20">
+      {images.map((src, index) => (
+        <div key={index} className="relative w-full h-0 pb-[100%]">
+          <img
+            src={src}
+            alt={`Image ${index}`}
+            layout="fill"
+            objectFit="cover"
+            className="absolute inset-0"
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+Gallery.propTypes = {
+  images: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+// Page d'accueil
+export default function Home() {
+  const images = [
+    'https://res.cloudinary.com/dxecll7yj/image/upload/v1723890954/lunette-de-soleil-bleu-plastique-recycle-bambou-organique-modele-sunglass_y6hjgy.webp',
+    'https://res.cloudinary.com/dxecll7yj/image/upload/v1723890954/s-l400_wwcznx.jpg',
+    'https://res.cloudinary.com/dxecll7yj/image/upload/v1723891749/_afflelou_core_07630629414754_afflelou_ma_model_65b7c48cb7174_s7br6d.png',
+    'https://res.cloudinary.com/dxecll7yj/image/upload/v1723891749/Hommes_udvaqf.webp',
+    'https://res.cloudinary.com/dxecll7yj/image/upload/v1723891748/categorie-par-defaut-layer_ccner4.jpg',
+    'https://res.cloudinary.com/dxecll7yj/image/upload/v1723891964/c42de44522eb3ae9f3724972a2da7550_ojynqc.jpg',
+    // Ajoutez plus d'images ici
+  ];
+
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-4xl font-bold text-center my-8">Nos créations</h1>
+      <Gallery images={images} />
+    </div>
   );
 }
