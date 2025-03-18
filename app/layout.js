416b4b8eacd8eab1d2005835290a22034b13b3b2
Melvyn <103742613+0xMelvyn@users.noreply.github.com>
2025-03-18 10:06:59 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -35,9 +35,8 @@ export default function RootLayout({ children }) {
         <meta property="twitter:image" content="/" />
         <meta property="twitter:url" content="" />
       </Head>
-    <Navbar />
       <body className={`font-Raleway ${raleway.className}`}>
-
+<Navbar />
       {children}
         
         
