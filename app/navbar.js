08e3188c41a845acd77f55dceaad286c1a7cd304
clcommunication <communication@charlesetlucie.fr>
2025-04-26 18:56:17 +0200

    Update navbar.js

--- app/navbar.js
+++ app/navbar.js
@@ -46,15 +46,15 @@ const Navbar = () => {
           href="/propos"
           className="hover:text-mustard transition duration-300 ease-in-out mx-4"
         >
+            Prendre RDV
+        </Link>
          <Link
             href="https://www.doctolib.fr/magasin-d-optique/toulouse/opticien-les-belles-branches"
             rel="noopener noreferrer"
             target="_blank"
             className="py-2 text-lg text-center bg-mustard transition duration-300 ease-in-out text-cool rounded-full px-4 hover:scale-105"
-            onClick={() => setIsOpen(false)}
-          >
-            Prendre RDV
-          </Link>
+            onClick={() => setIsOpen(false)}  
+
       </div>
       <div className="lg:hidden flex flex-1 justify-end">
         <button onClick={toggleMenu} className="hover:text-mustard focus:outline-none">
